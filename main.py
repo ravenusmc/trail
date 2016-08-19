fb21302ccaf0a4623a0915cf05023bbfb198d6c9
ravenusmc <mcuddy77@gmail.com>
2016-08-19 15:28:15 -0400

    added a main function

---
+++ main.py
@@ -0,0 +1,12 @@
+def main():
+  print("\033c")
+  print("{            The Oregon Trail             }")
+  print(" ----------------------------------------- ")
+  print("You may:")
+  print("1. Travel the trail")
+  print("2. Learn about the trail")
+  print("3. Quit")
+  choice = int(input("What is your choice? "))
+
+
+main()
