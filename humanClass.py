164968d3c50b57494fcd88b9092a0d3d6ff40673
ravenusmc <mcuddy77@gmail.com>
2016-09-10 15:24:03 -0400

    working on creating disease methods in human class

--- humanClass.py
+++ humanClass.py
@@ -1,4 +1,5 @@
 from wagon import *
+import random 
 
 class Human():
 
@@ -7,6 +8,7 @@ class Human():
     self.job = job 
     self.money = 0
     self.life = 30
+    self.infected = False 
 
   #This method will drain the life from each of the player objects a little bit each day. It's a tough 
   #journey. In order for the life to go back up, the wagon has got to stop. 
@@ -20,12 +22,44 @@ class Human():
 
   #This method will increase the life of the party when they are resting. 
   def lifeIncrease(self, wagon):
-    if wagon.ration == "Tons":
+    if wagon.ration == "Tons" and self.infected = False:
       self.life += 5
-    elif wagon.ration == "Normal":
+    elif wagon.ration == "Normal" and self.infected = False:
       self.life += 4
-    elif wagon.ration == "Meager":
+    elif wagon.ration == "Meager" and self.infected = False:
       self.life += 3
+    elif wagon.ration == "Tons" and self.infected = True:
+      self.life += 4
+    elif wagon.ration == "Normal" and self.infected = True:
+      self.life += 3
+    elif wagon.ration == "Meager" and self.infected = True:
+      self.life += 3
+
+
+  def diseaseInfection(self):
+    diseaseChance = random.randint(1, 10)
+    diseases = ["typhoid", "Yellow Fever", "cholera"]
+    diseaseName = random.randint(0,2)
+
+    if diseaseChance > 7 and self.infected == False:
+      disease = diseases[diseaseName]
+      self.infected = True 
+      print(self.name + " has been infected with: " + disease)
+
+  def rest(self):
+    healthyChance = random.randint(1,10)
+    if self.infected = True && healthyChance < 5: 
+      print(self.name + " is disease free!")
+
+
+
+
+
+
+mike = Human("Mike", "Banker")
+beth = Human("Beth", "Cook")
+mike.diseaseInfection()
+beth.diseaseInfection()
 
 
 
