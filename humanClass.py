fe24a21d54214736b2729319c631e3d453898f1e
ravenusmc <mcuddy77@gmail.com>
2016-09-10 16:04:00 -0400

    minor modifications to program

--- humanClass.py
+++ humanClass.py
@@ -42,40 +42,47 @@ class Human():
 
   #This method will assign a disease to an individual at a random point.
   def diseaseInfection(self):
-    diseaseChance = random.randint(1, 10)
+    diseaseChance = random.randint(1, 30)
     diseases = ["typhoid", "Yellow Fever", "cholera"]
     diseaseName = random.randint(0,2)
 
-    if diseaseChance > 7 and self.infected == False and self.alive == True:
+    if diseaseChance > 25 and self.infected == False and self.alive == True:
       disease = diseases[diseaseName]
       self.infected = True 
-      print(self.name + " has been infected with: " + disease)
+      print(self.name + " has been infected with: " + disease )
       print("You best stop the wagon and rest!")
-      input("Press Enter to continue! You best rest to help " + self.name)
+      input("Press Enter to continue! You best rest to help " + self.name )
 
   #This is the method which will slowly kill the disease infected party member if they do not rest. 
   def diseaseKilling(self):
     if self.infected == True and self.alive == True: 
       self.life -= 3
-    print("")
-    print(self.name + " is infected with illness.")
-    print("You best stop the wagon and rest!")
-    input("Press Enter to continue! You best rest to help " + self.name)
+      print("")
+      print(self.name + " is infected with illness.")
+      print("You best stop the wagon and rest!")
+      input("Press Enter to continue! You best rest to help " + self.name )
 
   #This method will work to heal the individual from their disease when the wagon is not moving. 
   def rest(self):
     healthyChance = random.randint(1,10)
     if self.infected == True and healthyChance < 5 and self.alive == True: 
       print(self.name + " is disease free!")
+      input("Hit Enter to continue!")
 
 
 
 # mike = Human("Mike", "Banker")
 # beth = Human("Beth", "Cook")
+# print(mike.infected)
+# print(beth.infected)
 # mike.diseaseInfection()
 # beth.diseaseInfection()
+# print(mike.infected)
+# print(beth.infected)
 # mike.diseaseKilling()
 # beth.diseaseKilling()
+# print(mike.infected)
+# print(beth.infected)
 
 
 
